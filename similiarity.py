f6dcac4a9550f70977f7b9406b4c3cdd51686a5e
wouterleeftink <wouterleeftink@gmail.com>
2018-10-16 20:17:20 +0200

    Update similiarity.py
    Changed to also include bands, code still runs very slow

--- similiarity.py
+++ similiarity.py
@@ -22,7 +22,6 @@ np.random.seed(42)
 
 user_movie = np.load("user_movie.npy")
 
-
 movie_users = dict() 
 
 for i in user_movie:
@@ -34,10 +33,11 @@ for i in user_movie:
         movie_users[m_id].add(u_id)  
 
 movies = list(movie_users)    
-unique_users = u_id + 1
     
     
 sig_dict = dict()
+
+unique_users = set()
 
 sig_len = 100
 for p in range(sig_len):
@@ -45,18 +45,34 @@ for p in range(sig_len):
     index = np.random.permutation(len(movies)) 
     for i in index:
         users_m = movie_users[ movies[i] ]
+        unique_users.update(users_m)
         for u in users_m:
             if u not in sig_dict:
-                sig_dict[u] = {i}
+                sig_dict[u] = [i]
             else:
                 if len(sig_dict[u]) == p+1:
                     break
                 else:
-                    sig_dict[u].add(i) 
-                
-                 
-#sig_mat = np.matrix(sig_list)
-    
+                    sig_dict[u].append(i) 
+                    
+sig_list = list()                   
+for u in list(unique_users).sort():
+    sig_list.append(sig_dict[u])
+sig_mat = np.matrix(sig_list)
+sig_mat = np.transpose(sig_mat)
+
+bucket_dict = dict()
+number_band = 10
+for b in range(number_band):
+    for u in range(unique_users):
+        bucket_id = hash(sig_mat[10*b:10*b+10,u].tostring())
+        if bucket_id not in bucket_dict:
+            bucket_dict[bucket_id] = [u]
+        else:
+            bucket_dict[bucket_id].append(u)
+
+bucket_dict = {k: v for k, v in bucket_dict.items() if len(v) > 1}
+
 #partsize = 100
 
 for u_combo in itertools.combinations(sig_dict, 2):
@@ -65,8 +81,9 @@ for u_combo in itertools.combinations(sig_dict, 2):
     set_b = sig_dict[u_combo[1]]
     #set_b = set([set_b[i:i+partsize] for i in range(len(set_b))][:-partsize])
     jdist = len(set_a & set_b) / len(set_a | set_b)
+    combos = list()
     if jdist > 0.5:
-        print(u_combo)
+        combos.append([u_combo, jdist])
         
         
 # https://www.learndatasci.com/tutorials/building-recommendation-engine-locality-sensitive-hashing-lsh-python/
