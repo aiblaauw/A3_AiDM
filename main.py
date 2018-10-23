12543761073d50ce075ea7fa2ba8574c6b27ec29
Astrid <aiblaauw@gmail.com>
2018-10-23 15:24:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,15 +16,16 @@ start_time = time.clock()
 import numpy as np
 import itertools
 import random
-
 import sys
 from scipy.sparse import csr_matrix, csc_matrix
+
+## randomseed = int(sys.argv[1])
+## filepath = sys.argv[2]
 
 np.random.seed(42)
 
 start_time = time.clock()
  
-
 user_movie = np.load("user_movie.npy")
 
 num_movies = len(np.unique(user_movie[:,1]))
@@ -61,18 +62,22 @@ for b in range(number_band):
         for combination in combinations:
             potential_combos.append(combination)
 
+combocount = 0
 for combination in potential_combos:
     vector_1 = user_movie[user_movie[:, 0] == combination[0]][:,1]
     vector_2 = user_movie[user_movie[:, 0] == combination[1]][:,1] 
     jdist = len(np.intersect1d(vector_1, vector_2)) / len(np.union1d(vector_1, vector_2))
     if jdist > 0.5:
-        print([combination, jdist])
-        combos.append([combination, jdist])
+        combocount += 1
+        print("\n--- %s minutes ---" %((time.clock()- start_time)/60))      
+        print([combocount, combination, jdist])
+        ## output = open("results.txt", "a")
+        ## output.write(str(combination[0]) + "," + (combination[1]))
+        ## output.close()
 
+        
 # https://www.learndatasci.com/tutorials/building-recommendation-engine-locality-sensitive-hashing-lsh-python/
 # http://nbviewer.jupyter.org/github/mattilyra/LSH/blob/master/examples/Introduction.ipynb
 # https://towardsdatascience.com/understanding-locality-sensitive-hashing-49f6d1f6134
-
-print(len(combos))
             
 print("\n--- %s minutes ---" %((time.clock()- start_time)/60))
